94d422cbdf63e989e6190df80f8359888d4cb163
Bobscorn <jwed606@aucklanduni.ac.nz>
2025-09-11 16:38:33 +1200

    wip: almost working serial comms with pc

--- firmware/raspy-mario/raspy-mario.cpp
+++ firmware/raspy-mario/raspy-mario.cpp
@@ -9,13 +9,12 @@
 #include "servo.h"
 #include "DAC.h"
 #include "tmp_audio.h"
+#include "comms.h"
 
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     // Put your timeout handler code in here
     return 0;
 }
-
-
 
 // UART defines
 // By default the stdout UART is `uart0`, so we will use the second one
@@ -29,7 +28,7 @@ int64_t alarm_callback(alarm_id_t id, void *user_data) {
 
 int main()
 {
-    stdio_init_all();
+    setup_comms();
 
     // Initialise the Wi-Fi chip
     if (cyw43_arch_init()) {
@@ -78,9 +77,19 @@ int main()
     // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
 
     while (true) {
+        // Test sending messages
+        Message msg;
+        msg.type = MessageType::PING;
+        send_message(msg);
+        sleep_ms(1000);
+        msg.type = MessageType::MESSAGE;
+        const char* test_str = "Hello from Raspy Mario!";
+        msg.text.length = strlen(test_str);
+        msg.text.text = (uint8_t*)test_str;
+        send_message(msg);
+        sleep_ms(1000);
+        stdio_flush();
         play_audio();
-        printf("Hello there, general kenobi!\n");
-        sleep_ms(2000);
         // set_servo_angle(0);
         // sleep_ms(3000);
         // set_servo_angle(180);
